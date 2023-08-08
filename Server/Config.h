fa6443f7facadc7912de3b0b4b4eb10555a829d6
Ilya Shkurko <apple.gh@mail.ru>
2023-08-08 14:27:41 +0300

    Update Config.h
    Add Encode times setting

--- Server/Config.h
+++ Server/Config.h
@@ -7,4 +7,5 @@ namespace CONFIG
 	inline constexpr size_t BUFLEN_DEFAULT = 1024;
 	inline constexpr int PORT_DEFAULT = 8888;
 	inline constexpr std::string IP_DEFAULT = "127.0.0.1";
+	inline constexpr int ENCODE_TIMES = 3;
 };
